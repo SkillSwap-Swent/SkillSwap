02a912297ef356ad7bdf6b93cedafac68bf8bf27
Layth <layth.armani@epfl,ch>
2025-10-05 13:32:16 +0200

    test: add basic user class tests

--- app/src/test/java/com/swent/skillswap/UserUnitTest.kt
+++ app/src/test/java/com/swent/skillswap/UserUnitTest.kt
@@ -4,33 +4,27 @@ import com.swent.skillswap.model.user.Availability
 import com.swent.skillswap.model.user.Skill
 import com.swent.skillswap.model.user.SkillName
 import com.swent.skillswap.model.user.User
-import org.junit.Assert.*
-import org.junit.Test
 import java.time.DayOfWeek
 import java.time.LocalTime
+import org.junit.Assert.*
+import org.junit.Test
 
-/**
- * User local unit test
- */
+/** User local unit test */
 class UserUnitTest {
 
   @Test
   fun createUserAndCheckProperties() {
-    val skill = Skill(SkillName.LINEAR_ALGEBRA,3f,"")
-    val availability = Availability(
-      DayOfWeek.MONDAY,
-      LocalTime.of(9, 0),
-      LocalTime.of(17, 0)
-    )
-    val user = User(
-      uid = "123",
-      username = "testuser",
-      email = "test@example.com",
-      profilePicture = "pic_url",
-      skillSet = setOf(skill),
-      rating = 4.5f,
-      availability = listOf(availability)
-    )
+    val skill = Skill(SkillName.LINEAR_ALGEBRA, 3f, "")
+    val availability = Availability(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(17, 0))
+    val user =
+        User(
+            uid = "123",
+            username = "testuser",
+            email = "test@example.com",
+            profilePicture = "pic_url",
+            skillSet = setOf(skill),
+            rating = 4.5f,
+            availability = listOf(availability))
 
     assertEquals("123", user.uid)
     assertEquals("testuser", user.username)
@@ -44,21 +38,17 @@ class UserUnitTest {
 
   @Test
   fun testUserEqualityAndCopy() {
-    val skill = Skill(SkillName.LINEAR_ALGEBRA,3f,"")
-    val availability = Availability(
-      DayOfWeek.TUESDAY,
-      LocalTime.of(10, 0),
-      LocalTime.of(18, 0)
-    )
-    val user1 = User(
-      uid = "abc",
-      username = "user1",
-      email = "user1@example.com",
-      profilePicture = "url1",
-      skillSet = setOf(skill),
-      rating = 5.0f,
-      availability = listOf(availability)
-    )
+    val skill = Skill(SkillName.LINEAR_ALGEBRA, 3f, "")
+    val availability = Availability(DayOfWeek.TUESDAY, LocalTime.of(10, 0), LocalTime.of(18, 0))
+    val user1 =
+        User(
+            uid = "abc",
+            username = "user1",
+            email = "user1@example.com",
+            profilePicture = "url1",
+            skillSet = setOf(skill),
+            rating = 5.0f,
+            availability = listOf(availability))
     val user2 = user1.copy()
     assertEquals(user1, user2)
     val user3 = user1.copy(username = "user2")
