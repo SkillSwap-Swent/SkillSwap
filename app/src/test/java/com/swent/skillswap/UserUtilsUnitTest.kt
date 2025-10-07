fcc5d90146a3b9ac6b3d5133153edb204b0149df
bidipopp <leonard.marti@epfl.ch>
2025-10-07 13:04:07 +0200

    Implement all unit tests for UserUtils

--- app/src/test/java/com/swent/skillswap/UserUtilsUnitTest.kt
+++ app/src/test/java/com/swent/skillswap/UserUtilsUnitTest.kt
@@ -1,25 +1,66 @@
 package com.swent.skillswap
 
 import com.swent.skillswap.model.user.*
-import org.junit.Assert.*
-import org.junit.Test
 import java.time.DayOfWeek
 import java.time.LocalTime
+import org.junit.Assert.*
+import org.junit.Test
 
 /** User utility unit test */
-class UserUtilsTest {
+class UserUtilsUnitTest {
 
     @Test
     fun correctSerializationOfSkill() {
         val skill = Skill(SkillName.LINEAR_ALGEBRA, 3f, "")
-        val serialized = serializeSkill(skill)
-        assertEquals(skill, deserializeSkill(serialized))
+        val serialized = serializeSingleSkill(skill)
+        assertEquals(skill, deserializeSingleSkill(serialized))
     }
 
     @Test
     fun correctSerializationOfAvailability() {
         val availability = Availability(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(17, 0))
-        val serialized = serializeAvailability(availability)
-        assertEquals("MONDAY,09:00,17:00", serialized)
+        val serialized = serializeSingleAvailability(availability)
+        assertEquals(availability, deserializeSingleAvailability(serialized))
+    }
+
+    @Test
+    fun correctSerializationOfSetOfSkills() {
+        val skill1 = Skill(SkillName.LINEAR_ALGEBRA, 3f, "qnfj3of")
+        val skill2 = Skill(SkillName.CHEMISTRY, 2f, "qnededefj3of")
+        val skills = setOf(skill1, skill2)
+        val serialized = serializeSkills(skills)
+        print(serialized + "\n")
+        assertEquals(skills, deserializeSkills(serialized))
+    }
+
+    @Test
+    fun correctSerializationOfListOfAvailabilities() {
+        val availability1 = Availability(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(17, 0))
+        val availability2 =
+            Availability(DayOfWeek.WEDNESDAY, LocalTime.of(10, 0), LocalTime.of(15, 0))
+        val availabilities = listOf(availability1, availability2)
+        val serialized = serializeAvailabilities(availabilities)
+        assertEquals(availabilities, deserializeAvailabilities(serialized))
+    }
+
+    @Test
+    fun correctSerializationOfUser() {
+        val skill1 = Skill(SkillName.LINEAR_ALGEBRA, 3f, "qnfj3of")
+        val skill2 = Skill(SkillName.CHEMISTRY, 2f, "qnededefj3of")
+
+        val availability = Availability(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(17, 0))
+        val user =
+            User(
+                uid = "123",
+                username = "testuser",
+                email = "test@example.com",
+                profilePicture = "pic_url",
+                skillSet = setOf(skill1, skill2),
+                rating = 4.5f,
+                availability = listOf(availability)
+            )
+
+        val serialized = serializeUser(user)
+        assertEquals(user, deserializeUser(serialized))
     }
 }
