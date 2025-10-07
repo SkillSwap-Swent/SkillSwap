1a8907da9fc6e83929b04bb7a12042105489daa8
bidipopp <leonard.marti@epfl.ch>
2025-10-07 19:44:14 +0200

    Add more tests to increase cover

--- app/src/test/java/com/swent/skillswap/UserUtilsUnitTest.kt
+++ app/src/test/java/com/swent/skillswap/UserUtilsUnitTest.kt
@@ -61,5 +61,43 @@ class UserUtilsUnitTest {
 
         val serialized = serializeUser(user)
         assertEquals(user, deserializeUser(serialized))
+
+        // Edge case: empty skill set and availability list
+        val user2 =
+            User(
+                uid = "",
+                username = "",
+                email = "",
+                profilePicture = "",
+                skillSet = setOf(),
+                rating = 0.0f,
+                availability = listOf()
+            )
+        val serialized2 = serializeUser(user2)
+        assertEquals(user2, deserializeUser(serialized2))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun moreEdgeCasesOnUserSerialization() {
+        val user =
+            User(
+                uid = "",
+                username = "",
+                email = "",
+                profilePicture = "",
+                skillSet = setOf(),
+                rating = Float.NaN, //illegal arg that should throw an exception
+                availability = listOf()
+            )
+        serializeUser(user)
+    }
+
+    @Test
+    fun illegalSerializableInstantiations_doNotThrow() {
+        SerializableSkill("", -1f, "desc")
+        SerializableSkill("qejndb", Float.NaN, "desc")
+        SerializableAvailability("", "09:00", "17:00")
+        SerializableAvailability("MONDAY", "", "17:00")
+        SerializableAvailability("MONDAY", "09:00", "")
     }
 }
