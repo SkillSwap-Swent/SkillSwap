dd446538e272582167528fe1dfbf8b92f97be067
Layth <layth.armani@epfl,ch>
2025-10-10 20:40:35 +0200

    fix(test): firestore write finishes before test ends
    This ensures that the emulator gets the requests for writing before terminating the test

--- app/src/androidTest/java/com/swent/skillswap/FirestoreDebug.kt
+++ app/src/androidTest/java/com/swent/skillswap/FirestoreDebug.kt
@@ -2,10 +2,13 @@ package com.swent.skillswap
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.gms.tasks.Tasks
 import com.swent.skillswap.utils.FirebaseEmulator
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
+import kotlin.text.set
 
 @RunWith(AndroidJUnit4::class)
 class FirestoreDebugTest {
@@ -18,11 +21,8 @@ class FirestoreDebugTest {
         val collection = firestore.collection("testCollection")
 
         val task = collection.document("testDocument").set(testDocument)
+        val result = Tasks.await(task, 5, TimeUnit.SECONDS)
+        assertTrue("Document should be written successfully", task.isSuccessful)
 
-        InstrumentationRegistry.getInstrumentation().runOnMainSync {
-            task.addOnCompleteListener {
-                assertTrue("Document should be written successfully", it.isSuccessful)
-            }
-        }
     }
 }
