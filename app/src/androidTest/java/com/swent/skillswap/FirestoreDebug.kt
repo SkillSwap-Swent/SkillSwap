6d45c155897196d3100b66a48b202aabefc81472
Layth <layth.armani@epfl,ch>
2025-10-10 20:48:38 +0200

    chore: format with ktfmt

--- app/src/androidTest/java/com/swent/skillswap/FirestoreDebug.kt
+++ app/src/androidTest/java/com/swent/skillswap/FirestoreDebug.kt
@@ -1,14 +1,13 @@
 package com.swent.skillswap
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import com.google.android.gms.tasks.Tasks
 import com.swent.skillswap.utils.FirebaseEmulator
+import java.util.concurrent.TimeUnit
+import kotlin.text.set
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.concurrent.TimeUnit
-import kotlin.text.set
 
 @RunWith(AndroidJUnit4::class)
 class FirestoreDebugTest {
@@ -23,6 +22,5 @@ class FirestoreDebugTest {
         val task = collection.document("testDocument").set(testDocument)
         val result = Tasks.await(task, 5, TimeUnit.SECONDS)
         assertTrue("Document should be written successfully", task.isSuccessful)
-
     }
 }
