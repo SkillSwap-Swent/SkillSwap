48b1d25372fd3225f90d1fc4594cac7a7f899a3c
Alex Magnus <a2005magnus@gmail.com>
2025-10-10 10:40:49 +0200

    feature(post) implement offer data class

---
+++ app/src/main/java/com/swent/skillswap/model/post/Offer.kt
@@ -0,0 +1,24 @@
+/** Credits: Code: Alexander Magnus */
+package com.swent.skillswap.model.post
+
+import com.google.firebase.Timestamp
+import com.swent.skillswap.model.tags.EveryTag
+
+data class Offer(
+    override val uid: String,
+    override val title: String,
+    override val description: String,
+    override val ownerId: String,
+    override val tags: List<EveryTag>,
+    override val paymentMethods: List<PaymentMethod>,
+    override val expiry: Timestamp,
+    override val creation: Timestamp,
+    override val status: PostStatus,
+    override val media: List<String>,
+) : Post {
+    override val type: PostType
+        get() = PostType.OFFER
+
+    // TODO: implement proper validation logic
+    // https://github.com/orgs/SkillSwap-Swent/projects/1/views/2?filterQuery=&pane=issue&itemId=132697400
+}
