6b8a703ed08b16a00b56019d6fa6aa4f97ad6e00
Alex Magnus <a2005magnus@gmail.com>
2025-10-13 14:16:49 +0200

    fix(post) fix documentToPost() helper incorrectly casting Strings to Enums

--- app/src/main/java/com/swent/skillswap/model/tags/EveryTag.kt
+++ app/src/main/java/com/swent/skillswap/model/tags/EveryTag.kt
@@ -1,3 +1,18 @@
 package com.swent.skillswap.model.tags
 
-sealed interface EveryTag
+sealed interface EveryTag {
+    // TODO: This code is kind of hacky. Maybe find a better way.
+    companion object {
+        fun valueOf(value: String): EveryTag {
+            return try {
+                PostTag.valueOf(value)
+            } catch (_: IllegalArgumentException) {
+                try {
+                    SkillTag.valueOf(value)
+                } catch (_: IllegalArgumentException) {
+                    throw IllegalArgumentException("Unknown tag: $value")
+                }
+            }
+        }
+    }
+}
