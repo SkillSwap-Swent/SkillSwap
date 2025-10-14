b891f42941dbb7b557b8c0e8f3fe81f1c996c50f
Alex Magnus <a2005magnus@gmail.com>
2025-10-14 10:48:24 +0200

    fix(tags) remove dependency on implementation from EveryTag.kt by creating a TagRegistry.kt

--- app/src/main/java/com/swent/skillswap/model/tags/EveryTag.kt
+++ app/src/main/java/com/swent/skillswap/model/tags/EveryTag.kt
@@ -1,18 +1,7 @@
 package com.swent.skillswap.model.tags
 
 sealed interface EveryTag {
-    // TODO: This code is kind of hacky. Maybe find a better way.
     companion object {
-        fun valueOf(value: String): EveryTag {
-            return try {
-                PostTag.valueOf(value)
-            } catch (_: IllegalArgumentException) {
-                try {
-                    SkillTag.valueOf(value)
-                } catch (_: IllegalArgumentException) {
-                    throw IllegalArgumentException("Unknown tag: $value")
-                }
-            }
-        }
+        fun valueOf(value: String): EveryTag = TagRegistry.fromValue(value)
     }
 }
