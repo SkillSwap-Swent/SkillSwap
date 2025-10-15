813161e013deff0e864a66c866d33d985757071c
Layth <layth.armani@epfl,ch>
2025-10-15 15:34:09 +0200

    feat(ui): add placeholder chat screen

---
+++ app/src/main/java/com/swent/skillswap/ui/chat/ChatScreen.kt
@@ -0,0 +1,13 @@
+package com.swent.skillswap.ui.chat
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+/*TODO Implement chat screen UI, placeholder for now*/
+
+@Composable
+fun ChatScreen() {
+
+    Text("Placeholder Chat Screen")
+}
