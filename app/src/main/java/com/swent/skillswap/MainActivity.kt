67ea05f98f8dfad91dbf06cb2f268a28907d517c
egros <eliott.grosjean@epfl.ch>
2025-10-30 14:21:54 +0100

    feat(auth-backend): kftmt format

--- app/src/main/java/com/swent/skillswap/MainActivity.kt
+++ app/src/main/java/com/swent/skillswap/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.google.firebase.auth.FirebaseAuth
 import com.swent.skillswap.model.navigation.NavigationBottomBarModel
 import com.swent.skillswap.resources.C
 import com.swent.skillswap.ui.chat.ChatScreen
