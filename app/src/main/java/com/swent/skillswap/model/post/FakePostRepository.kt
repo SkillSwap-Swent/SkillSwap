46b6411c481daf19afa890e67cd31424088d00f4
BarbyBard <a2005magnus@gmail.com>
2025-11-03 16:03:51 +0100

    refactor(FakeRepository) update to use new exhaustive paymentMethod enums and set for tags

--- app/src/main/java/com/swent/skillswap/model/post/FakePostRepository.kt
+++ app/src/main/java/com/swent/skillswap/model/post/FakePostRepository.kt
@@ -56,8 +56,8 @@ class FakePostRepository : PostRepository {
         type: PostType,
         titleContains: String,
         ownerId: String,
-        paymentMethods: List<PaymentMethod>,
-        tags: List<EveryTag>,
+        paymentMethod: PaymentMethod,
+        tags: Set<EveryTag>,
         status: PostStatus?
     ): List<Post> {
         getMultiplePostsCalls++
