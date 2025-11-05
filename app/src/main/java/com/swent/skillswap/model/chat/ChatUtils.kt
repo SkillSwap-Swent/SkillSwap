e4971e9a6fd26b2c6fe2e6a1d6df939f80d1dbde
bidipopp <leonard.marti@epfl.ch>
2025-11-05 15:51:32 +0100

    Add ChatUtils file to manage serialization

---
+++ app/src/main/java/com/swent/skillswap/model/chat/ChatUtils.kt
@@ -0,0 +1,19 @@
+package com.swent.skillswap.model.chat
+
+/**
+ * Utility functions for serializing and deserializing chat messages.
+ */
+fun serializeMessage(message: Message): String {
+    return "${message.id}|${message.senderId}|${message.content}|${message.timestamp}"
+}
+
+fun deserializeMessage(data: String): Message {
+    return data.split("|").let {
+        Message(
+            id = it[0],
+            senderId = it[1],
+            content = it[2],
+            timestamp = it[3].toLong()
+        )
+    }
+}
