af8b44a09ce88dea0409a7a0adc296b983e00fb5
BarbyBard <a2005magnus@gmail.com>
2025-11-11 12:55:46 +0100

    chore: format

--- app/src/main/java/com/swent/skillswap/ui/theme/Theme.kt
+++ app/src/main/java/com/swent/skillswap/ui/theme/Theme.kt
@@ -1,14 +1,9 @@
 package com.swent.skillswap.ui.theme
 
-import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.colorResource
-import androidx.core.view.WindowCompat
 import com.swent.skillswap.R
 
 @Composable
