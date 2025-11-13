31dc48d8ac0127e78030da91e5c041850c199821
Topaze17 <eliott.grosjean@epfl.ch>
2025-11-13 13:09:21 +0100

    refactor(ui-profile): change SkillPill.kt to be button and not box

--- app/src/main/java/com/swent/skillswap/ui/utils/SkillPill.kt
+++ app/src/main/java/com/swent/skillswap/ui/utils/SkillPill.kt
@@ -1,23 +1,28 @@
 package com.swent.skillswap.ui.utils
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.swent.skillswap.model.tags.SkillTag
 import com.swent.skillswap.ui.auth.CreateAccountTags
 
+@Preview(showBackground = true)
 @Composable
-fun SkillPill(skill: SkillTag, isSelected: Boolean, onClick: (skill: SkillTag) -> Unit) {
+fun SkillPill(
+    skill: SkillTag = SkillTag.MACHINE_DESIGN,
+    isSelected: Boolean = false,
+    onClick: (skill: SkillTag) -> Unit = { skill -> skill }
+) {
     val backgroundColor =
         if (isSelected) {
             MaterialTheme.colorScheme.secondary
@@ -30,13 +35,11 @@ fun SkillPill(skill: SkillTag, isSelected: Boolean, onClick: (skill: SkillTag) -
         } else {
             MaterialTheme.colorScheme.onSecondaryContainer
         }
-
-    Box(
-        modifier =
-            Modifier.background(backgroundColor, shape = RoundedCornerShape(50))
-                .clickable { onClick(skill) }
-                .testTag(CreateAccountTags.SKILL_CHIP_PREFIX + skill.name)
-                .padding(horizontal = 11.dp, vertical = 6.dp)
+    Button(
+        onClick = { onClick(skill) },
+        colors = ButtonDefaults.buttonColors(containerColor = backgroundColor),
+        contentPadding = PaddingValues(11.dp, 0.dp),
+        modifier = Modifier.height(34.dp).testTag(CreateAccountTags.SKILL_CHIP_PREFIX + skill.name)
     ) {
         Text(
             text = skill.name, // TODO: make enum names user-friendly
