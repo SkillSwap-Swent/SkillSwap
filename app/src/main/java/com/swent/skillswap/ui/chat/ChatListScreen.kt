a3d60064bdacaac337306819b930505cb5fbcd3c
marcosantonalvarez <marcosantonalvarezz@gmail.com>
2025-11-20 06:37:37 +0100

    chore(format): ktfmtFormat

--- app/src/main/java/com/swent/skillswap/ui/chat/ChatListScreen.kt
+++ app/src/main/java/com/swent/skillswap/ui/chat/ChatListScreen.kt
@@ -10,18 +10,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.swent.skillswap.model.chat.Chat
-import com.swent.skillswap.model.chat.ChatRepository
-import com.swent.skillswap.model.chat.Message
-import com.swent.skillswap.model.post.PostRepository
 import com.swent.skillswap.model.post.PostType
-import com.swent.skillswap.model.user.UserRepositery
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 @Composable
 fun ChatListScreen(viewModel: ChatListViewModel = viewModel(), onPostClick: (String) -> Unit = {}) {
