7a0facc675da3bb09d78a29dc681dcbcdb22014f
bidipopp <leonard.marti@epfl.ch>
2025-11-26 11:04:30 +0100

    [minor] Refractor CloudReferences in CloudConfig.kt

--- app/src/main/java/com/swent/skillswap/firebase/CloudConfig.kt
+++ app/src/main/java/com/swent/skillswap/firebase/CloudConfig.kt
@@ -1,6 +1,6 @@
 package com.swent.skillswap.firebase
 
 object CloudReferences {
-    const val FEED_PICTURES_PATH = "images/"
+    const val FEED_PICTURES_PATH = "feed/"
     const val PROFILE_PICTURES_PATH = "profile_pictures/"
 }
