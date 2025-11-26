489c1c7b627f4591e284e179a40e17e520a1a4fe
bidipopp <leonard.marti@epfl.ch>
2025-11-26 16:19:00 +0100

    [minor] add a values field to CloudReferences

--- app/src/main/java/com/swent/skillswap/firebase/CloudConfig.kt
+++ app/src/main/java/com/swent/skillswap/firebase/CloudConfig.kt
@@ -3,4 +3,6 @@ package com.swent.skillswap.firebase
 object CloudReferences {
     const val FEED_PICTURES_PATH = "feed/"
     const val PROFILE_PICTURES_PATH = "profile_pictures/"
+
+    val values = setOf(FEED_PICTURES_PATH, PROFILE_PICTURES_PATH)
 }
