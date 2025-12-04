b6f60301542ccbf67c90e6df277e3ca9c91562fe
Topaze17 <eliott.grosjean@epfl.ch>
2025-12-04 13:36:52 +0100

    fix(chat-acceptChatModel): fake repo needed correct implementation of new function in interface

--- app/src/test/java/com/swent/skillswap/post/FeedControllerTest.kt
+++ app/src/test/java/com/swent/skillswap/post/FeedControllerTest.kt
@@ -33,6 +33,20 @@ open class FeedControllerTest : PostDataClassTest() {
 
         override suspend fun getChatsOfCurrentUser(relatedPostType: PostType) =
             chats[relatedPostType] ?: emptyList()
+
+        override suspend fun getPendingChatsOfCurrentUser(relatedPostType: PostType): List<Chat> {
+            // JUST HERE FOR OVERRIDE REASON
+            return emptyList()
+        }
+
+        override suspend fun isOwnerOfRelatedPost(chat: Chat): Boolean {
+            // JUST HERE FOR OVERRIDE REASON
+            return false
+        }
+
+        override suspend fun acceptAPostReplyChat(chat: Chat) {
+            // JUST HERE FOR OVERRIDE REASON
+        }
     }
 
     suspend fun initController(): Pair<FakePostRepository, FeedController> {
