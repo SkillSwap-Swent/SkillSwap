fd114682922528abb1df71bd9a97c1730a937191
marcosantonalvarez <marcosantonalvarezz@gmail.com>
2025-12-04 17:32:21 +0100

    fix(user): update fake user repositories to override new updateRating method

--- app/src/androidTest/java/com/swent/skillswap/ui/chat/ChatListScreenTest.kt
+++ app/src/androidTest/java/com/swent/skillswap/ui/chat/ChatListScreenTest.kt
@@ -60,6 +60,8 @@ class ChatListScreenTest {
         override suspend fun userExists(userId: String) = users.containsKey(userId)
 
         override suspend fun updateFcmToken(userId: String, fcmToken: String) {}
+
+        override suspend fun updateRating(userId: String, incomingRating: Float) {}
     }
 
     private class FakePostRepository(private val posts: Map<String, Post>) : PostRepository {
